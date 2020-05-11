353c558d41df4bff7da7bd6566854cd0acc57a9f
JoshLampen <joshlampen2@gmail.com>
2020-05-11 20:03:54 +0000

    Added ability to POST new long url in submission form; downloaded body-parser library to help accomplish this

--- express_server.js
+++ express_server.js
@@ -4,16 +4,28 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  
+}
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/new", (req, res) => {
