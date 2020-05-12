879daebebb680a7af1d4ce142cf092c90b7f6c9a
JoshLampen <joshlampen2@gmail.com>
2020-05-12 18:07:37 +0000

    Have created the initial html for the register page as well as the code for GET /register; header now dynamically changes based on if the person is registered

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
+// imports and setup
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 
@@ -11,6 +12,7 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 
+// constants --> move to other file
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -27,42 +29,51 @@ const generateRandomString = () => {
 };
 
 
+// get homepage
 app.get("/urls", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
-    urls: urlDatabase
+    urls: urlDatabase,
+    registered: true
   };
   res.render("urls_index", templateVars);
 });
 
+// post new url to homepage
 app.post("/urls", (req, res) => {
   const key = generateRandomString();
   urlDatabase[key] = req.body.longURL;
   res.redirect(`/urls/${key}`);
 });
 
+// get page for creating new url
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
+    registered: true
   };
   res.render("urls_new", templateVars);
 });
 
+// delete existing short URL from homepage
 app.post("/urls/:shortURL/delete", (req, res) => {
   const key = req.params.shortURL;
   delete urlDatabase[key];
   res.redirect("/urls");
 });
 
+// get page for existing short URL from homepage
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    registered: true
   };
   res.render("urls_show", templateVars);
 });
 
+// replace existing short URL on homepage with new long URL
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const newLongURL = req.body.longURL;
@@ -70,20 +81,37 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
+// get long URL page from short URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// 'login' and add a new username
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
+// 'logout' and delete a username
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
+
+// get the 'register' page
+app.get("/register", (req, res) => {
+  let templateVars = {
+    registered: false
+  };
+  res.render("urls_register", templateVars);
+});
+
+// register a new email and password from the 'register' page
+// app.post("/register", (req, res) => {
+//   res.cookie("email", req.body.email);
+//   res.cookie("password", req.body.password);
+// });
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
