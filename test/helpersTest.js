58189418d792cbcfd78b99572a0a154c68f3fabc
JoshLampen <joshlampen2@gmail.com>
2020-05-13 19:14:41 +0000

    Added automated tests for the getUserURLs() helper function, using Mocha and Chai

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, getUserURLs } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -12,6 +12,12 @@ const testUsers = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
+};
+
+const testURLDatabase = {
+  b6UTxQ: { longURL: "https://www.firstexample.com", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.secondexample.com", userID: "b5o2xJ" },
+  b5oR46: { longURL: "https://www.thirdexample.com", userID: "aJ48lW" }
 };
 
 describe('getUserByEmail', function() {
@@ -28,5 +34,21 @@ describe('getUserByEmail', function() {
   it('should return undefined if the email is not in the database', function() {
     const user = getUserByEmail("user3@example.com", testUsers);
     assert.strictEqual(user, undefined);
+  });
+});
+
+describe('getUserURLs', function() {
+  it('should return all short URLs that match the userID', function() {
+    const userURLs = getUserURLs("aJ48lW", testURLDatabase);
+    const expectedOutput = {
+      b6UTxQ: "https://www.firstexample.com",
+      b5oR46: "https://www.thirdexample.com"
+    };
+    assert.deepEqual(userURLs, expectedOutput);
+  });
+
+  it('should return an empty object if none of the short URLs match the userID', function() {
+    const userURLs = getUserURLs("hb6Ils", testURLDatabase);
+    assert.deepEqual(userURLs, {});
   });
 });
